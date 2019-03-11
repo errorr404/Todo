09ec20a70d825e7ee84f7d54c8b2de13187968f9
errorr404 <dkbishwash@gmail.com>
2019-03-11 18:04:01 +0530

    added redux dev-tool-extension

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore,applyMiddleware } from "redux";
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
 import reducer from "./reducers";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const store = createStore(reducer,applyMiddleware(thunk));
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
