658fe000c15e5395aa35f25d121ab045b71f815e
errorr404 <dkbishwash@gmail.com>
2019-03-11 17:43:11 +0530

    added redux-thunk and setup api calls

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,41 @@
 import {ADD_TODO,UPDATE_TODO} from '../constant'
+import axios from 'axios'
 
-export const addTodo = (name)=>{
+export const addTodo = (name) =>{
+    return dispatch =>{
+        axios.post('http://localhost:5000/api/post_todo',{name:name}).then(res=>{
+            if(res.status===200){
+                dispatch(addTodoSuccess(name))
+            }
+        })
+    }
+}
+
+const addTodoSuccess = (name)=>{
     const action = {
         type:ADD_TODO,
         name
     }
     console.log('addTodo is called...')
+    return action
+}
+
+export const setInitialTodo = ()=>{
+    return dispatch =>{
+        axios.get('http://localhost:5000/api/get_todo').then(res=>{
+            if(res.status===200){
+                console.log(res.data)
+                dispatch(setInitialTodoSuccess(res.data.todo))
+            }
+        })
+    }
+  
+}
+
+const setInitialTodoSuccess = (name)=>{
+    const action = {
+        type:ADD_TODO,
+        name
+    }
     return action
 }
--- client/src/component/TodoList.js
+++ client/src/component/TodoList.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
+import {setInitialTodo} from '../actions/index'
 class TodoList extends React.Component {
+
+  componentDidMount(){
+    this.props.setInitialTodo()
+  }
   render() {
-      var todos
+    console.log(this.props.todos)
+      var todos=[]
   if(this.props.todos){
       todos=this.props.todos
   }
   else todos=[]
     return (
       <div>
-        {     todos.map((todo,idx)=>{
-                    return <li key={idx}>{todo}</li>
+        {     todos.map((todo)=>{
+                    return <li key={todo._id}>{todo.name}</li>
                 })}
       </div>
     );
@@ -25,5 +31,5 @@ function mapStateToProps(state) {
 
 export default connect(
   mapStateToProps,
-  null
+  {setInitialTodo}
 )(TodoList);
