10178eab9943a5249b916d1943fa3510623b1ecb
errorr404 <dkbishwash@gmail.com>
2019-03-12 17:48:28 +0530

    added alert

--- client/src/component/CreateTodo.js
+++ client/src/component/CreateTodo.js
@@ -2,10 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { addTodo } from "../actions";
 import TextField from "@material-ui/core/TextField";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
-import Select from '@material-ui/core/Select';
-import NativeSelect from '@material-ui/core/NativeSelect';
 
 class CreateTodo extends React.Component {
   constructor() {
@@ -16,24 +14,38 @@ class CreateTodo extends React.Component {
   }
   handleAddTodo = e => {
     e.preventDefault();
-    this.props.addTodo(this.state.name);
+    if(this.state.name && this.state.name.length>0){
+      this.props.addTodo(this.state.name)
+      this.setState({name:""})
+    }
+    else{
+      alert('Enter the Todo first')
+    }
+   
   };
   render() {
     console.log(this.state.name);
     return (
       <div>
-          <form onSubmit={this.handleAddTodo} >
-        <FormControl variant="outlined">
-          <TextField
-            id="outlined-name"
-            label="Task Name"
-            value={this.state.name}
-            onChange={e => this.setState({ name: e.target.value })}
-            margin="normal"
-            variant="outlined"
-          />
-        <Button variant="contained" color="primary" onClick={this.handleAddTodo} > Add the task</Button>
-        </FormControl>
+        <form onSubmit={this.handleAddTodo}>
+          <FormControl variant="outlined">
+            <TextField
+              id="outlined-name"
+              label="Task Name"
+              value={this.state.name}
+              onChange={e => this.setState({ name: e.target.value })}
+              margin="normal"
+              variant="outlined"
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleAddTodo}
+            >
+              {" "}
+              Add the task
+            </Button>
+          </FormControl>
         </form>
       </div>
     );
