caad31c4d53a5c1bd9deeae9ab1dba55f5475397
errorr404 <dkbishwash@gmail.com>
2019-03-12 11:27:56 +0530

    changed logic from filter to map for updating the todo in reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,18 +1,25 @@
-import {ADD_TODO,UPDATE_TODO} from '../constant'
+import { ADD_TODO, UPDATE_TODO } from "../constant";
 
-const todoFunc = (state=[],action) =>{
-    let todo = null
-    switch(action.type){
-        case ADD_TODO:
-        todo = [...state,action.name]
-        console.log('in add todo reducer')
-        return todo
+const todoFunc = (state = [], action) => {
+  let todo = null;
+  switch (action.type) {
+    case ADD_TODO:
+      todo = [...state, action.name];
+      console.log("in add todo reducer");
+      return todo;
 
-        case UPDATE_TODO:
-        todo = state.filter(item=>item._id!==action.payload._id)
-        var newTodo = [...todo,action.payload]
-        return newTodo
-    }
-}
+    case UPDATE_TODO:
+      todo = state.map(item => {
+        if (item._id === action.payload._id) {
+          item.name = action.payload.name;
+          item.priority = action.payload.priority;
+          item.completed = action.payload.completed;
+          return item;
+        } else return item;
+      });
+      console.log("todo in reducer", todo);
+      return todo;
+  }
+};
 
-export default todoFunc
+export default todoFunc;
