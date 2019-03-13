c1f724110e52d1cebea00c6b60f323e27e6ea793
errorr404 <dkbishwash@gmail.com>
2019-03-13 12:02:47 +0530

    added modal and update task logic

--- client/src/component/TodoList.js
+++ client/src/component/TodoList.js
@@ -7,6 +7,11 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import Modal from "react-responsive-modal";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+
 import { setInitialTodo, updateTodo, deleteTodo } from "../actions/index";
 
 const CustomTableCell = withStyles(theme => ({
@@ -20,10 +25,70 @@ const CustomTableCell = withStyles(theme => ({
 }))(TableCell);
 
 class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showEditName: false,
+      showEditPriority: false,
+      openName: false,
+      openPriority: false,
+      currentId: "",
+      currentName: "",
+      currentPriority: "",
+      currentCompleted:""
+    };
+  }
+
+  handleModalName = (id, name, priority,completed) => {
+    this.setState({
+      currentId: id,
+      currentName: name,
+      openName: true,
+      currentPriority: priority,
+      currentCompleted:completed
+    });
+  };
+
+  handleModalPriority = (id, priority, name,completed) => {
+    this.setState({
+      currentId: id,
+      currentPriority: priority,
+      openPriority: true,
+      currentName: name,
+      currentCompleted:completed
+    });
+  };
+
   handleDelete = id => {
     this.props.deleteTodo(id);
   };
 
+  handleUpdateTask = e => {
+    e.preventDefault();
+    console.log('in handle task...')
+    console.log(
+      this.state.currentId,
+      this.state.currentName,
+      this.state.currentPriority,
+      this.state.currentCompleted
+    );
+    this.props.updateTodo(this.state.currentId,this.state.currentName,this.state.currentCompleted,this.state.currentPriority)
+    this.setState({currentId:'',currentName:'',currentPriority:'',openName:false})
+  };
+  handleUpdatePriority = e => {
+    this.props.updateTodo(
+      this.state.currentId,
+      this.state.currentName,
+      this.state.currentCompleted,
+      this.state.currentPriority
+    );
+    this.setState({
+      currentId: "",
+      currentName: "",
+      currentPriority: "",
+      openPriority: false
+    });
+  };
   handleUpdate = obj => {
     console.log(obj);
     var completed = !obj.completed;
@@ -33,13 +98,79 @@ class TodoList extends React.Component {
     this.props.setInitialTodo();
   }
   render() {
-    console.log(this.props.todos);
+    console.log(this.state);
+    // console.log(this.props.todos);
     var todos = [];
     if (this.props.todos) {
       todos = this.props.todos;
     } else todos = [];
     return (
       <Paper>
+        <Modal
+          open={this.state.openName}
+          onClose={e =>
+            this.setState({ openName: false, currentId: "", currentName: "" })
+          }
+          center
+        >
+          <h2>Edit the task</h2>
+          <form onSubmit={this.handleUpdateTask}>
+            <FormControl variant="outlined">
+              <TextField
+                id="outlined-name"
+                label="Task Name"
+                value={this.state.currentName}
+                onChange={e => this.setState({ currentName: e.target.value })}
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={this.handleUpdateTask}
+              >
+                {" "}
+                Add the task
+              </Button>
+            </FormControl>
+          </form>
+        </Modal>
+        <Modal
+          open={this.state.openPriority}
+          onClose={e =>
+            this.setState({
+              openPriority: false,
+              currentId: "",
+              currentPriority: ""
+            })
+          }
+          center
+        >
+          <h2>Change the Priority</h2>
+          <form onSubmit={this.handleUpdatePriority}>
+            <FormControl variant="outlined">
+              <TextField
+                id="outlined-name"
+                label="Priority"
+                value={this.state.currentPriority}
+                onChange={e =>
+                  this.setState({ currentPriority: e.target.value })
+                }
+                margin="normal"
+                variant="outlined"
+              />
+              <p>Eg:-High,Low,Medium</p>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={this.handleUpdatePriority}
+              >
+                {" "}
+                Add the task
+              </Button>
+            </FormControl>
+          </form>
+        </Modal>
         <Table>
           <TableHead>
             <TableRow>
@@ -53,10 +184,48 @@ class TodoList extends React.Component {
           <TableBody>
             {todos.map(todo => (
               <TableRow key={todo._id}>
-                <CustomTableCell component="th" scope="row">
+                <CustomTableCell
+                  component="th"
+                  scope="row"
+                  onMouseOver={e => this.setState({ showEditName: true })}
+                  onMouseLeave={e => this.setState({ showEditName: false })}
+                >
                   {todo.name}
+                  {this.state.showEditName ? (
+                    <i
+                      className="fas fa-edit"
+                      style={{ cursor: "pointer" }}
+                      onClick={e =>
+                        this.handleModalName(todo._id, todo.name, todo.priority,todo.completed)
+                      }
+                    />
+                  ) : (
+                    ""
+                  )}
                 </CustomTableCell>
-                <CustomTableCell align="right">{todo.priority}</CustomTableCell>
+                <CustomTableCell
+                  align="right"
+                  onMouseOver={e => this.setState({ showEditPriority: true })}
+                  onMouseLeave={e => this.setState({ showEditPriority: false })}
+                >
+                  {todo.priority}
+                  {this.state.showEditPriority ? (
+                    <i
+                      className="fas fa-edit"
+                      style={{ cursor: "pointer" }}
+                      onClick={e =>
+                        this.handleModalPriority(
+                          todo._id,
+                          todo.priority,
+                          todo.name,
+                          todo.completed
+                        )
+                      }
+                    />
+                  ) : (
+                    ""
+                  )}
+                </CustomTableCell>
                 <CustomTableCell align="right">
                   {todo.completed === true ? "Completed" : "Pending"}
                 </CustomTableCell>
