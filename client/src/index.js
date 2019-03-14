f3929f81566368dad456d493050de3106ec6c10e
errorr404 <dkbishwash@gmail.com>
2019-03-14 12:39:49 +0530

    removed redux-devtool

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import reducer from "./reducers";
 import "./index.css";
@@ -11,7 +10,7 @@ import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  applyMiddleware(thunk)
 );
 
 ReactDOM.render(
